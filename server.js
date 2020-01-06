2b7663c2921fe6e3e7d8384151db941204948122
keenan560 <keenan560@gmail.com>
2020-01-05 23:43:55 -0500

    added expression sessions and express handlebars, need to build out the login slogin works

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const Port = process.env.port || 3000;
 const path = require("path");
-const public = path.join(__dirname, 'public');
+const views = path.join(__dirname, 'views');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const bcrypt = require("bcrypt");
+const hbs = require('express-handlebars');
+
 
 
 const mysql = require('mysql');
@@ -16,30 +17,112 @@ const connection = mysql.createConnection({
     database: 'trade_ninja_db'
 });
 
+const two_hours = 1000 * 60 * 60 * 2;
+const {
+    Port = 3000,
+    NODE_ENV = 'development',
+    SESS_NAME = 'sid',
+    SESS_SECRET = 'ninja',
+    SESS_LIFETIME = two_hours
+} = process.env
+
 connection.connect();
 
+const IN_PROD = NODE_ENV === 'production';
 const app = express();
 
-app.use('/', express.static(public));
+app.use('/', express.static(views));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(session({secret: "ninja", cookie: {maxAge: 60000}}));
-
-
-app.get("/", (req, res) => {
-    if (req.session.views) {
-        req.s
+app.use(session(
+    {
+        name: SESS_NAME,
+        resave: false,
+        secret: SESS_SECRET,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: two_hours,
+            sameSite: true,
+            secure: IN_PROD
+        }
     }
-    res.sendFile(path.join(public, 'login.html'));
+));
+
+const redirectLogin = (req, res, next) => {
+    if (!req.session.user) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
+const redirectHome = (req, res, next) => {
+    if (req.session.user) {
+        res.redirect('/dashboard');
+    } else {
+        next();
+    }
+}
+
+app.use((req, res, next) => {
+    const { user } = req.session;
+    if (user) {
+
+        res.locals.user = req.session.user;
+    }
+    next();
+})
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
+
+
+
+// ***** ROUTES ******
+app.get("/", redirectHome, (req, res) => {
+    const { user } = req.session;
+    console.log(user);
+    res.render('login');
 })
 
-app.get("/login", (req, res) => {
-    res.sendFile(path.join(public, 'login.html'));
+app.get("/login", redirectHome, (req, res) => {
+    res.render('login')
+
 });
 
-app.get("/register", (req, res) => {
-    res.sendFile(path.join(public, 'register.html'));
+app.get("/register", redirectHome, (req, res) => {
+    res.render('register');
+
 });
+
+app.get("/dashboard", redirectLogin, (req, res) => {
+    const { user } = res.locals;
+    console.log(req.session)
+    res.render('dashboard', { firstName: user.first_name });
+
+});
+
+app.get("/about", (req, res) => {
+    res.render('about');
+});
+
+app.get("/portfolio", redirectLogin, (req, res) => {
+    const { user } = res.locals;
+    res.render('portfolio', { title: "Holdings", userName: user.user_name });
+})
+
+app.get("/cash", redirectLogin, (req, res) => {
+    const { user } = res.locals;
+    res.render('cash', { title: 'Cash', userName: user.user_name });
+});
+
+app.get("/history", redirectLogin, (req, res) => {
+    const { user } = res.locals;
+    res.render('history', { title: 'History', userName: user.user_name });
+});
+
+app.get("/trade", redirectLogin, (req, res) => {
+    const {user} = res.locals;
+    res.render('trade', {title: 'Trade', userName: user.user_name});
+})
 
 app.post("/users/email", (req, res) => {
     let email = req.body.email;
@@ -71,7 +154,7 @@ app.post("/users/username", (req, res) => {
 });
 
 
-app.post("/users", async (req, res) => {
+app.post("/users", redirectHome, async (req, res) => {
 
     try {
         const salt = await bcrypt.genSalt(10);
@@ -99,8 +182,6 @@ app.post("/auth", async (req, res) => {
     connection.query(`SELECT * FROM users WHERE user_name = '${req.body.username}'`, async (err, results) => {
         if (err) throw err;
 
-        console.log(results);
-
         if (results.length === 0) {
             return res.send('Username does not exist');
         }
@@ -108,8 +189,9 @@ app.post("/auth", async (req, res) => {
         if (results.length > 0) {
             try {
                 if (await bcrypt.compare(req.body.password, results[0].password)) {
-
-                    res.send("Login successful!");
+                    req.session.user = results[0];
+                    console.log(req.session.user.user_name);
+                    res.send("Login Successful!")
                 } else {
 
                     res.send("Invalid credentials!");
@@ -128,6 +210,17 @@ app.post("/auth", async (req, res) => {
 
 })
 
+
+app.post('/logout', redirectLogin, (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect("/dashboard");
+        }
+
+        res.clearCookie(SESS_NAME);
+        res.send("Ninja vanish!");
+    })
+})
 
 
 
