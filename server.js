a9125d6ce4399d719fa541264dbb20c0a72d9c40
keenan560 <keenan560@gmail.com>
2020-04-14 20:58:06 -0500

    added a ninja dispoal button

--- server.js
+++ server.js
@@ -899,26 +899,7 @@ app.post('/logout', redirectLogin, (req, res) => {
     })
     
 })
-// connection.connect();
-// function handleDisconnect(connection) {
-//     connection.on('error', function (err) {
-//         if (!err.fatal) {
-//             return;
-//         }
-
-//         if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-//             throw err;
-//         }
-
-//         console.log('Re-connecting lost connection: ' + err.stack);
-
-//         connection = mysql.createConnection(connection.config);
-//         handleDisconnect(connection);
-//         connection.connect();
-//     });
-// }
-
-// handleDisconnect(connection);
+ 
 
 connection.connect(function (err) {
     if (err) {
